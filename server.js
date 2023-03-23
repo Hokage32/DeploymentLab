997683673dcf77f72a15e83594ca39d3b95ea3c6
Levi Raoelina <levi_r34@yahoo.com>
2023-03-23 15:03:20 -0600

    css change:

--- server.js
+++ server.js
@@ -9,7 +9,20 @@ app.use(express.json())
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'e8672a55788b4d138e7689a8ef50c5e2',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.post('/bananas', (req,res) => {
+    res.stta
+})
 
 
 
